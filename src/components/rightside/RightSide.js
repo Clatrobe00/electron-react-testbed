7e5d54c1c794d933cb0c33cf7b0ae7f54a82d41c
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-06-21 17:04:25 -0700

    create rightside component

---
+++ src/components/rightside/RightSide.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSide = (props) => {
+    return (
+        <div className='container'>
+            <p>{props.notes}</p>
+        </div>
+    )
+}
+
+export default RightSide;
